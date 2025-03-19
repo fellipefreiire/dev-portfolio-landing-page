12bbafc23624a8754c4f88f72d74fdb1c808f8a1
fellipefreiire <fellipefreiire3@gmail.com>
2025-03-19 13:57:12 -0300

    chore: tape section

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,35 @@
+import StarIcon from '@/assets/star.svg'
+
+const words = [
+  'Performant',
+  'Accessible',
+  'Secure',
+  'Interactive',
+  'Scalable',
+  'User Friendly',
+  'Responsive',
+  'Maintainable',
+  'Search Optimized',
+  'Usable',
+  'Reliable',
+]
 export function Tape() {
-  return 'Tape'
+  return (
+    <div className="overflow-x-clip py-16 lg:py-24">
+      <div className="-mx-1 -rotate-3 bg-gradient-to-r from-emerald-300 to-sky-400">
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex flex-none gap-4 py-3">
+            {words.map((word) => (
+              <div key={word} className="inline-flex items-center gap-4">
+                <span className="text-sm font-extrabold text-gray-900 uppercase">
+                  {word}
+                </span>
+                <StarIcon className="size-6 -rotate-12 text-gray-900" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
