fd0524751e45ee032edcb061c0351712c09707ee
fellipefreiire <fellipefreiire3@gmail.com>
2025-03-21 20:10:54 -0300

    chore: sections done

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,3 +1,36 @@
+import { ArrowUpRight } from 'lucide-react'
+import grainImage from '@/assets/grain.jpg'
+
 export function Contact() {
-  return 'Contact'
+  return (
+    <div className="py-16 pt-12 lg:py-24 lg:pt-20">
+      <div className="container">
+        <div className="relative z-0 overflow-hidden rounded-3xl bg-gradient-to-r from-emerald-300 to-sky-400 px-10 py-8 text-center text-gray-900 md:text-left">
+          <div
+            className="absolute inset-0 -z-30 opacity-5"
+            style={{
+              backgroundImage: `url(${grainImage.src})`,
+            }}
+          />
+          <div className="flex flex-col items-center gap-8 md:flex-row md:gap-16">
+            <div>
+              <h2 className="font-serif text-2xl md:text-3xl">
+                Let&apos;s create something amazing together
+              </h2>
+              <p className="mt-2 text-sm md:text-base">
+                Ready to bring your next project to life? Let&apos;s connect and
+                discuss how I can help you achieve your goals.
+              </p>
+            </div>
+            <div>
+              <button className="inline-flex h-12 w-max items-center justify-center gap-2 rounded-xl border border-gray-900 bg-gray-900 px-6 text-white">
+                <span className="font-semibold">Contact Me</span>
+                <ArrowUpRight className="size-4" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,45 @@
+import { ArrowUpRight } from 'lucide-react'
+
+const footerLinks = [
+  {
+    title: 'YouTube',
+    href: '#',
+  },
+  {
+    title: 'Twitter',
+    href: '',
+  },
+  {
+    title: 'Instagram',
+    href: '#',
+  },
+  {
+    title: 'LinkedIn',
+    href: '#',
+  },
+]
+
 export function Footer() {
-  return 'Footer'
+  return (
+    <footer className="relative -z-10 overflow-x-clip">
+      <div className="absolute bottom-0 left-1/2 -z-10 h-[400px] w-[1600px] -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)]" />
+      <div className="container">
+        <div className="flex flex-col items-center gap-8 border-t border-white/15 py-6 text-sm md:flex-row md:justify-between">
+          <div className="text-white/40">&copy; 2024. All rights reserved.</div>
+          <nav className="items-centers flex flex-col gap-8 md:flex-row">
+            {footerLinks.map((link) => (
+              <a
+                key={link.title}
+                href={link.href}
+                className="inline-flex items-center justify-center gap-1.5"
+              >
+                <span className="font-semibold">{link.title}</span>
+                <ArrowUpRight className="size-4" />
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  )
 }
