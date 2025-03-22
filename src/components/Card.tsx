d74f0d64d8638198da404eabc9f547b2ba2a3a36
fellipefreiire <fellipefreiire3@gmail.com>
2025-03-21 21:59:50 -0300

    chore: projects section animation

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,20 @@
 import grainImage from '@/assets/grain.jpg'
 import { cn } from '@/utils/cn'
+import type { ComponentPropsWithoutRef } from 'react'
 
 type CardProps = {
   children: React.ReactNode
   className?: string
-}
+} & ComponentPropsWithoutRef<'div'>
 
-export function Card({ children, className }: CardProps) {
+export function Card({ children, className, ...props }: CardProps) {
   return (
     <div
       className={cn(
         "relative z-0 overflow-hidden rounded-3xl bg-gray-800 after:pointer-events-none after:absolute after:inset-0 after:-z-10 after:rounded-3xl after:outline-2 after:-outline-offset-2 after:outline-white/20 after:content-['']",
         className,
       )}
+      {...props}
     >
       <div
         className="absolute inset-0 -z-30 opacity-5"
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,8 +2,8 @@ import { NavLink } from '@/components/NavLink'
 
 export function Header() {
   return (
-    <div className="fixed top-3 flex w-full items-center justify-center">
-      <nav className="flex gap-1 rounded-full border border-white/15 p-0.5">
+    <div className="fixed top-3 z-30 flex w-full items-center justify-center">
+      <nav className="flex gap-1 rounded-full border border-white/15 bg-gray-800 p-0.5">
         <NavLink
           href="/"
           className="nav-item data-[current=true]:hover:text-gray-90 data-[current=true]:bg-white data-[current=true]:text-gray-900 data-[current=true]:hover:bg-white/70"
